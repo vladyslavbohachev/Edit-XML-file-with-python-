97cad5edbaa130da6422c36d30c527e158ab9c7e
Vladyslav Bohachev <94163529+vladyslavbohachev@users.noreply.github.com>
2022-05-24 09:03:39 +0200

    coderedesign

--- main.py
+++ main.py
@@ -1,110 +1,106 @@
 #!/usr/bin/python
-import os
+import datetime, os
 import shutil
 import xml.etree.ElementTree as ET
 
-rootdir = 'X:/'
-moddir = 'X:/1_modified'
-candir = 'X:/1_cancelled'
-importdir = 'X:/1_import'
+rootdir = r'X:/'
+importdir = r'X:/import'
+
+moddir = r'X:/modified'
+candir = r'X:/cancelled'
+acpdir = r'X:/BackupRAW'
+nowdir = 'X:/BackupRAW/' + str(datetime.date.today())
 
 def check_dir():
-    if not os.path.exists(moddir):
-        os.makedirs(moddir)
-    if not os.path.exists(candir):
-        os.makedirs(candir)
-    if not os.path.exists(importdir):
-        os.makedirs(importdir)
+    directoryList = [importdir, moddir, candir, acpdir, nowdir]
+    for items in directoryList:
+        os.makedirs(items, exist_ok=True)
 def clear_data():
-    filterMod = [filename for filename in os.listdir(rootdir) if filename.__contains__('modified')]
-    filterCan = [filename for filename in os.listdir(rootdir) if filename.__contains__('cancelled')]
+    filterMod = [filename for filename in os.listdir(rootdir) if filename.__contains__(str('modified'))]
+    filterCan = [filename for filename in os.listdir(rootdir) if filename.__contains__(str('cancelled'))]
+    filterAcp = [filename for filename in os.listdir(rootdir) if filename.__contains__(str('accepted'))]
+
     for filename in filterMod:
         path_to_file = os.path.join(rootdir, filename)
         shutil.move(path_to_file, moddir)
     for filename in filterCan:
         path_to_file = os.path.join(rootdir, filename)
         shutil.move(path_to_file, candir)
-def configure():
+    for filename in filterAcp:
+        path_to_file = os.path.join(rootdir, filename)
+        shutil.copy(path_to_file, nowdir)
+
+def setup():
     for root, directories, filenames in os.walk(rootdir):
         for filename in filenames:
             if filename.endswith('.xml'):
-                tree = ET.parse(os.path.join(rootdir,filename))
+                tree = ET.parse(os.path.join(rootdir, filename))
                 root = tree.getroot()
-                extner = []
-                for geext in root.findall('NK/AK/SD/EXTNR'):
-                    extner.append(geext.text)
-                    if str(extner).__contains__('0104'):
+                getExternnumber = []
+                for getExternnumberToText in root.findall('NK/AK/SD/EXTNR'):
+                    getExternnumber.append(getExternnumberToText.text)
+                    if str(getExternnumber).startswith('010') and str(getExternnumber).__contains__('0104') or str(getExternnumber).__contains__('0105'):
                         continue
                     else:
-                        for (id, METER1) in enumerate(root.findall('NK/AK/SD/FD/METER1')):
-                            METER1.set('ID', str(id))
+                        #Set ID's to relevant datasets
+                        for (sid, setStellplaetzeID) in enumerate(root.findall('NK/AK/SD/FD/METER1')):
+                            setStellplaetzeID.set('SID', str(sid))
                         tree.write(os.path.join(rootdir, filename))
-                        for (id_n, EXTNER) in enumerate(root.findall('NK/AK/SD/EXTNR')):
-                            EXTNER.set('NR', str(id_n))
+                        for (eid, setExternemummerID) in enumerate(root.findall('NK/AK/SD/EXTNR')):
+                            setExternemummerID.set('EID', str(eid))
                         tree.write(os.path.join(rootdir, filename))
-                        for (id_exttarn, tarn) in enumerate(root.findall('NK/AK/SD/EXTTANR')):
-                            tarn.set('ANR', str(id_exttarn))
+                        for (enid, setExternetransportnummerID) in enumerate(root.findall('NK/AK/SD/EXTTANR')):
+                            setExternetransportnummerID.set('ENID', str(enid))
                         tree.write(os.path.join(rootdir, filename))
                         for eml in root.findall('NK/AK/SD/EXTTANR'):
                             newval = ET.SubElement(eml, None)
                             newval.text = ''
                         ET.dump(tree)
-                        find_nr = [(x.find('NR')) for x in tree.findall('.//NR/..')]
-                        info = []
-                        for j in root.findall('NK/AK/SD/INFO/ABSINFO1'):
-                            split = j.text.split('fp')[0].split('Stellplätze: ')[1]
-                            info.append(split)
-                        meter1 = []
-                        for i in root.findall('NK/AK/SD/FD/METER1'):
-                            meter1.append(i.text)
-                        test = tree.findall('NK/AK/SD/FD/METER1[@ID]')
-                        for counter in range(len(find_nr)):
-                            print(f'OK - - - {filename}: ist', test[counter].text, ' soll', info[counter])
-                            if test[counter].text != info[counter] or test[counter] == '0.000':
-                                test[counter].text = str(info[counter])
-                                tree.write(os.path.join(rootdir, filename))
-                        EXTNR_1 = []
-                        for d in root.findall('NK/AK/SD/EXTNR'):
-                            EXTNR_1.append(d.text)
-                        exttarn = []
-                        for s in root.findall('NK/AK/SD/EXTTANR'):
-                            exttarn.append(s)
-                        tarn = tree.findall('NK/AK/SD/EXTTANR[@ANR]')
-                        for counter in range(len(find_nr)):
-                            print('Das ist EXTTARN', tarn[counter].text, ' soll', EXTNR_1[counter])
-                            if tarn[counter] == exttarn[counter].text:
-                                continue
-                            else:
-                                tarn[counter].text = str(EXTNR_1[counter])
-                                tree.write(os.path.join(rootdir, filename))
-                        extner = []
-                        for l in root.findall('NK/AK/SD/EXTNR'):
-                            extner.append(l.text)
-                        num = []
-                        for q in root.findall('NK/AK/SD/INFO/ABSINFO1'):
-                            split_num = q.text.split('Transportnummer: ')[1].split('\\nSAP')[0]
-                            num.append(split_num)
-                        num_list = tree.findall('NK/AK/SD/EXTNR[@NR]')
-                        for counter in range(len(find_nr)):
-                            print('Das ist die EXTNR', num_list[counter].text, ' soll', num[counter])
-                            if num_list[counter].text != num[counter]:
-                                num_list[counter].text = str(num[counter])
+                        #count shipments in order
+                        getAllShipments = [(x.find('NR')) for x in tree.findall('.//NR/..')]
+                        # Splits
+                        SplitStellplaetze = []
+                        SplitTransportnummer = []
+                        for splitcounter in root.findall('NK/AK/SD/INFO/ABSINFO1'):
+                            splitone = splitcounter.text.split('fp')[0].split('Stellplätze: ')[1]
+                            splittwo = splitcounter.text.split('Transportnummer: ')[1].split('\\nSAP')[0]
+                            SplitStellplaetze.append(splitone)
+                            SplitTransportnummer.append(splittwo)
+                        # Get Data
+                        getExternemummerData = []
+                        for i in root.findall('NK/AK/SD/EXTNR'):
+                            getExternemummerData.append(i.text)
+                        getExternetransportnummerData = []
+                        for j in root.findall('NK/AK/SD/EXTTANR'):
+                            getExternetransportnummerData.append(j)
+                        # Get Data from ID's
+                        getStellplaetzeID = tree.findall('NK/AK/SD/FD/METER1[@SID]')
+                        getExternetransportnummerID = tree.findall('NK/AK/SD/EXTTANR[@ENID]')
+                        getExternemummerID = tree.findall('NK/AK/SD/EXTNR[@EID]')
+                        for counter in range(len(getAllShipments)):
+                            if getStellplaetzeID[counter].text != SplitStellplaetze[counter] or getStellplaetzeID[counter] == '0.000' \
+                                    and getExternetransportnummerID[counter] != getExternetransportnummerData[counter].text \
+                                    and getExternemummerID[counter].text != SplitTransportnummer[counter]:
+                                getStellplaetzeID[counter].text = str(SplitStellplaetze[counter])
+                                getExternetransportnummerID[counter].text = str(getExternemummerData[counter])
+                                getExternemummerID[counter].text = str(SplitTransportnummer[counter])
                                 tree.write(os.path.join(rootdir, filename))
                         for id in root.findall('NK/AK/SD/FD/METER1'):
-                            del (id.attrib['ID'])
+                            del (id.attrib['SID'])
                         tree.write(os.path.join(rootdir, filename))
                         for id in root.findall('NK/AK/SD/EXTNR'):
-                            del (id.attrib['NR'])
+                            del (id.attrib['EID'])
                         tree.write(os.path.join(rootdir, filename))
                         for id in root.findall('NK/AK/SD/EXTTANR'):
-                            del (id.attrib['ANR'])
+                            del (id.attrib['ENID'])
                         tree.write(os.path.join(rootdir, filename))
-        filterfinal = [filename for filename in os.listdir(rootdir) if filename.__contains__('accepted')]
+        filterfinal = [filename for filename in os.listdir(rootdir) if filename.__contains__(str('accepted'))]
         for filename in filterfinal:
             path_to_import = os.path.join(rootdir, filename)
             shutil.move(path_to_import, importdir)
 def main():
     check_dir()
     clear_data()
+    setup()
 if __name__ == '__main__':
     main()
