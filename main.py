519dbdbe0da57fb67f922965c957ddf85099be3f
Vladyslav Bohachev <94163529+vladyslavbohachev@users.noreply.github.com>
2022-05-27 13:43:58 +0200

    update
    update final 27.05.2022

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
 import datetime
 import os
 import shutil
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as Et
 
 rootdir = r'X:/'
 importdir = r'X:/import'
-
 modifiedDir = r'X:/modified'
 cancelledDir = r'X:/cancelled'
 acceptedDir = r'X:/BackupRAW'
@@ -14,8 +12,8 @@ dailyBackupDir = 'X:/BackupRAW/' + str(datetime.date.today())
 
 
 def check_dir():
-    directoryList = [importdir, modifiedDir, cancelledDir, acceptedDir, dailyBackupDir]
-    for items in directoryList:
+    directorylist = [importdir, modifiedDir, cancelledDir, acceptedDir, dailyBackupDir]
+    for items in directorylist:
         os.makedirs(items, exist_ok=True)
 
 
@@ -30,66 +28,69 @@ def clear_data():
 
 def setup():
     for root, dirs, files in os.walk(rootdir):
-        if rootdir == None:
-            print('there are no files to transmit')
+        if rootdir is None:
             exit(0)
         else:
             for filename in files:
                 if filename.endswith('.xml'):
-                    tree = ET.parse(os.path.join(rootdir,filename))
+                    tree = Et.parse(os.path.join(rootdir, filename))
                     root = tree.getroot()
-                    getExternnumber = []
-                    for getExternnumberToText in root.findall('NK/AK/SD/EXTNR'):
-                        getExternnumber.append(getExternnumberToText.text)
-                        if str(getExternnumber).startswith('010'):
+                    getexternnumber = []
+                    for getexternnumberToText in root.findall('NK/AK/SD/EXTNR'):
+                        getexternnumber.append(getexternnumberToText.text)
+                        if str(getexternnumber).startswith('010'):
                             continue
                         else:
-                            rootList = ['NK/AK/SD/FD/METER1', 'NK/AK/SD/EXTNR', 'NK/AK/SD/EXTTANR']
-                            setIDList = ['setStellplaetzeID', 'setExternemummerID', 'setExternetransportnummerID']
-                            idList = ['SID', 'EID', 'ENID']
+                            rootlist = ['NK/AK/SD/FD/METER1', 'NK/AK/SD/EXTNR', 'NK/AK/SD/EXTTANR']
+                            setidlist = ['setStellplaetzeID', 'setExternemummerID', 'setExternetransportnummerID']
+                            idlist = ['SID', 'EID', 'ENID']
                             rootid = ['sid', 'eid', 'enid']
-                            for i in range(len(rootList)):
-                                for (rootid[i], setIDList[i]) in enumerate(root.findall(rootList[i])):
-                                    setIDList[i].set(idList[i], str(rootid[i]))
+                            for i in range(len(rootlist)):
+                                for (rootid[i], setidlist[i]) in enumerate(root.findall(rootlist[i])):
+                                    setidlist[i].set(idlist[i], str(rootid[i]))
                                 tree.write(os.path.join(rootdir, filename))
                             for eml in root.findall('NK/AK/SD/EXTTANR'):
-                                newval = ET.SubElement(eml, None)
+                                newval = Et.SubElement(eml, None)
                                 newval.text = ''
                             tree.write(os.path.join(rootdir, filename))
-                            SplitStellplaetze = []
-                            SplitTransportnummer = []
+                            splitstp = []
+                            splittransnum = []
                             for splitcounter in root.findall('NK/AK/SD/INFO/ABSINFO1'):
                                 splitone = splitcounter.text.split('fp')[0].split('Stellplätze: ')[1]
                                 splittwo = splitcounter.text.split('Transportnummer: ')[1].split('\\nSAP')[0]
-                                SplitStellplaetze.append(splitone)
-                                SplitTransportnummer.append(splittwo)
-                            getExternemummerData = []
+                                splitstp.append(splitone)
+                                splittransnum.append(splittwo)
+                            getexternemummerdata = []
                             for i in root.findall('NK/AK/SD/EXTNR'):
-                                getExternemummerData.append(i.text)
-                            getExternetransportnummerData = []
+                                getexternemummerdata.append(i.text)
+                            getexternetransportnummerdata = []
                             for j in root.findall('NK/AK/SD/EXTTANR'):
-                                getExternetransportnummerData.append(j)
-                            getStellplaetzeID = tree.findall('NK/AK/SD/FD/METER1[@SID]')
-                            getExternetransportnummerID = tree.findall('NK/AK/SD/EXTTANR[@ENID]')
-                            getExternemummerID = tree.findall('NK/AK/SD/EXTNR[@EID]')
+                                getexternetransportnummerdata.append(j)
+                            getstpeid = tree.findall('NK/AK/SD/FD/METER1[@SID]')
+                            getexttransnumid = tree.findall('NK/AK/SD/EXTTANR[@ENID]')
+                            getextnemid = tree.findall('NK/AK/SD/EXTNR[@EID]')
                             for counter in range(len([(x.find('NR')) for x in tree.findall('.//NR/..')])):
-                                if getStellplaetzeID[counter].text != SplitStellplaetze[counter] or getStellplaetzeID[counter] == '0.000' \
-                                        and getExternetransportnummerID[counter] != getExternetransportnummerData[counter].text \
-                                        and getExternemummerID[counter].text != SplitTransportnummer[counter]:
-                                    getStellplaetzeID[counter].text = str(SplitStellplaetze[counter])
-                                    getExternetransportnummerID[counter].text = str(getExternemummerData[counter])
-                                    getExternemummerID[counter].text = str(SplitTransportnummer[counter])
+                                if getstpeid[counter].text != splitstp[counter] or getstpeid[counter] == '0.000' \
+                                        and getexttransnumid[counter] != getexternetransportnummerdata[counter].text \
+                                        and getextnemid[counter].text != splittransnum[counter]:
+                                    getstpeid[counter].text = str(splitstp[counter])
+                                    getexttransnumid[counter].text = str(getexternemummerdata[counter])
+                                    getextnemid[counter].text = str(splittransnum[counter])
                                     tree.write(os.path.join(rootdir, filename))
-                            for i in range(len(rootList)):
-                                for id in root.findall(rootList[i]):
-                                    del (id.attrib[idList[i]])
+                            for i in range(len(rootlist)):
+                                for delid in root.findall(rootlist[i]):
+                                    del (delid.attrib[idlist[i]])
                                 tree.write(os.path.join(rootdir, filename))
             for filename in [filename for filename in os.listdir(rootdir) if filename.__contains__(str('accepted'))]:
                 shutil.move(os.path.join(rootdir, filename), os.path.join(importdir, filename))
         return None
+
+
 def main():
     check_dir()
     clear_data()
     setup()
+
+
 if __name__ == '__main__':
     main()
